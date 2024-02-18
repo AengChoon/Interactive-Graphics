cb930fa8b482e6574d468aea80823a8cd57695fb
AengChoon <kevin1131@naver.com>
2024-02-19 02:45:31 +0900

    Step1, Step2 Commit

---
+++ Transformations/SimpleShader.vert
@@ -0,0 +1,9 @@
+#version 460
+
+layout(location = 0) in vec3 Position;
+uniform mat4 ModelViewProjectionMatrix;
+
+void main()
+{
+	gl_Position = ModelViewProjectionMatrix * vec4(Position, 1.0);
+}
--- Transformations/src/main.cpp
+++ Transformations/src/main.cpp
@@ -1,4 +1,3 @@
-#include <random>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <GL/GL.h>
@@ -10,25 +9,70 @@
 constexpr int Width = 600;
 constexpr int Height = 600;
 
+GLuint VertexArray;
+GLuint VertexBuffer;
+
+cy::TriMesh Object;
+cy::GLSLProgram Program;
+GLuint ProgramID;
+
+cy::Vec3f Position {0.0f, 30.0f, 60.0f};
+cy::Matrix4f ProjectionMatrix;
+
+void Idle()
+{
+	glutPostRedisplay();
+}
+
+void Initialize()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	ProjectionMatrix = cy::Matrix4f::Perspective(cy::ToRadians(40.0f), static_cast<float>(Width) / static_cast<float>(Height), 0.1f, 1000.0f);
+
+	Object.LoadFromFileObj("assets/teapot.obj", true);
+
+	Program.BuildFiles("SimpleShader.vert", "SimpleShader.frag");
+	ProgramID = Program.GetID();
+
+	glGenVertexArrays(1, &VertexArray);
+	glBindVertexArray(VertexArray);
+
+	glGenBuffers(1, &VertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cy::Vec3f) * Object.NV(), &Object.V(0), GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+	glBindVertexArray(0);
+}
+
 void Render()
 {
-	std::random_device RandomDevice;
-	std::uniform_real_distribution<float> Distribution{0.0f, 1.0f};
-
-	glClearColor(Distribution(RandomDevice), Distribution(RandomDevice), Distribution(RandomDevice), 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	const cy::Matrix4f ViewMatrix = cy::Matrix4f::View(Position, {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f});
+	const cy::Matrix4f ModelMatrix = cy::Matrix4f::Scale(1.0f);
+	cy::Matrix4f ModelViewProjectionMatrix = ProjectionMatrix * ViewMatrix * ModelMatrix;
+	glUniformMatrix4fv(glGetUniformLocation(ProgramID, "ModelViewProjectionMatrix"), 1, GL_FALSE, &ModelViewProjectionMatrix(0, 0));
+
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(VertexArray);
+	glUseProgram(ProgramID);
+	glDrawArrays(GL_POINTS, 0, sizeof(cy::Vec3f) * Object.NV());
 	glutSwapBuffers();
 }
 
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv); //to initialize GLUT
+	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutInitWindowSize(Width, Height);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Transformations");
-	glutDisplayFunc(Render);
 	glewInit();
+	glutDisplayFunc(Render);
+	glutIdleFunc(Idle);
+	Initialize();
 	glutMainLoop();
 	return 0;
 }
